821725d5bae3227cb419338df8c9515ea3b4a8d1
Jhicken16 <jhicken16@gmail.com>
2024-01-02 11:47:35 +0000

    added a new route for health check endpoint.

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+app.get("/health", (req, res) => { 
+
+  res.sendStatus(200); 
+
+}); 
+
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
